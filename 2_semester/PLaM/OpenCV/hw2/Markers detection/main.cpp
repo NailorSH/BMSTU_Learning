ea9c1e48109c37edf1f9e911b74096e97b5511dd
Nail Shiyatov <nailshiyatov.04@mail.ru>
2023-09-19 16:25:09 +0300

    Some changes

--- 2_semester/PLaM/OpenCV/hw2/Markers detection/main.cpp
+++ 2_semester/PLaM/OpenCV/hw2/Markers detection/main.cpp
@@ -1,239 +1,113 @@
-#include <opencv2/highgui.hpp>
-#include <opencv2/aruco.hpp>
 #include <iostream>
-#include "aruco_samples_utility.hpp"
+#include <vector>
+#include <cmath>
 
 using namespace std;
-using namespace cv;
 
-namespace {
-    const char* about = "Basic marker detection";
-
-//! [aruco_detect_markers_keys]
-    const char* keys  =
-            "{d        |       | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
-            "DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, "
-            "DICT_6X6_50=8, DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12,"
-            "DICT_7X7_100=13, DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16,"
-            "DICT_APRILTAG_16h5=17, DICT_APRILTAG_25h9=18, DICT_APRILTAG_36h10=19, DICT_APRILTAG_36h11=20}"
-            "{cd       |       | Input file with custom dictionary }"
-            "{v        |       | Input from video or image file, if ommited, input comes from camera }"
-            "{ci       | 0     | Camera id if input doesnt come from video (-v) }"
-            "{c        |       | Camera intrinsic parameters. Needed for camera pose }"
-            "{l        | 0.1   | Marker side length (in meters). Needed for correct scale in camera pose }"
-            "{dp       |       | File of marker detector parameters }"
-            "{r        |       | show rejected candidates too }"
-            "{refine   |       | Corner refinement: CORNER_REFINE_NONE=0, CORNER_REFINE_SUBPIX=1,"
-            "CORNER_REFINE_CONTOUR=2, CORNER_REFINE_APRILTAG=3}";
-}
-//! [aruco_detect_markers_keys]
-
-string pointToString(Point2f point, int level) {
-    string result = "{\n";
-    for (int i = 0; i < level + 1; i++) {
-        result += "    ";
+class VandermondeMatrix {
+public:
+    // Конструктор принимает вектор коэффициентов αi и размерность матрицы m x n
+    VandermondeMatrix(const vector<double>& coefficients, size_t m, size_t n)
+            : coefficients_(coefficients), m_(m), n_(n) {
+        computeElements();
     }
-    result += "\"x\" : " + to_string(point.x) + ",\n";
-    for (int i = 0; i < level + 1; i++) {
-        result += "    ";
-    }
-    result += "\"y\" : " + to_string(point.y) + "\n";
-    for (int i = 0; i < level; i++) {
-        result += "    ";
-    }
-    result += "}";
-    return result;
-}
 
-string vectorToString(vector<Point2f> points, int level) {
-    string result = "[\n";
-    for (int i = 0; i < points.size(); i++) {
-        for (int j = 0; j < level + 1; j++) {
-            result += "    ";
+    // Оператор () для доступа к элементам матрицы по индексам (i, j)
+    double operator()(size_t i, size_t j) const {
+        return elements_[i][j];
+    }
+
+    // Константный двунаправленный итератор для перебора всех элементов матрицы
+    class ConstIterator {
+    public:
+        ConstIterator(const VandermondeMatrix& matrix, size_t row, size_t col)
+                : matrix_(matrix), row_(row), col_(col) {}
+
+        // Операторы сравнения для итераторов
+        bool operator!=(const ConstIterator& other) const {
+            return row_ != other.row_ || col_ != other.col_;
         }
-        result += pointToString(points[i], level + 1);
-        if (i < points.size() - 1) {
-            result += ",";
+
+        bool operator==(const ConstIterator& other) const {
+            return row_ == other.row_ && col_ == other.col_;
         }
-        result += "\n";
-    }
-    for (int i = 0; i < level; i++) {
-        result += "    ";
-    }
-    result += "]";
-    return result;
-}
 
-int main(int argc, char *argv[]) {
-//    cv::Mat markerImage;
-//    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-//    cv::aruco::generateImageMarker(dictionary, 24, 200, markerImage, 1);
-//    cv::imwrite("marker24.png", markerImage);
-
-    CommandLineParser parser(argc, argv, keys);
-    parser.about(about);
-
-    if(argc < 2) {
-        parser.printMessage();
-        return 0;
-    }
-
-    bool showRejected = parser.has("r");
-    bool estimatePose = parser.has("c");
-    float markerLength = parser.get<float>("l");
-
-    aruco::DetectorParameters detectorParams;
-    if(parser.has("dp")) {
-        FileStorage fs(parser.get<string>("dp"), FileStorage::READ);
-        bool readOk = detectorParams.readDetectorParameters(fs.root());
-        if(!readOk) {
-            cerr << "Invalid detector parameters file" << endl;
-            return 0;
+        // Оператор инкремента для перемещения итератора вперед
+        ConstIterator& operator++() {
+            if (col_ < matrix_.n_ - 1) {
+                ++col_;
+            } else {
+                ++row_;
+                col_ = 0;
+            }
+            return *this;
         }
-    }
 
-    if (parser.has("refine")) {
-        //override cornerRefinementMethod read from config file
-        detectorParams.cornerRefinementMethod = parser.get<aruco::CornerRefineMethod>("refine");
-    }
-    std::cout << "Corner refinement method (0: None, 1: Subpixel, 2:contour, 3: AprilTag 2): " << (int)detectorParams.cornerRefinementMethod << std::endl;
-
-    int camId = parser.get<int>("ci");
-
-    String video;
-    if(parser.has("v")) {
-        video = parser.get<String>("v");
-    }
-
-    if(!parser.check()) {
-        parser.printErrors();
-        return 0;
-    }
-
-    aruco::Dictionary dictionary = aruco::getPredefinedDictionary(0);
-    if (parser.has("d")) {
-        int dictionaryId = parser.get<int>("d");
-        dictionary = aruco::getPredefinedDictionary(aruco::PredefinedDictionaryType(dictionaryId));
-    }
-    else if (parser.has("cd")) {
-        FileStorage fs(parser.get<std::string>("cd"), FileStorage::READ);
-        bool readOk = dictionary.aruco::Dictionary::readDictionary(fs.root());
-        if(!readOk) {
-            std::cerr << "Invalid dictionary file" << std::endl;
-            return 0;
+        // Оператор декремента для перемещения итератора назад
+        ConstIterator& operator--() {
+            if (col_ > 0) {
+                --col_;
+            } else {
+                --row_;
+                col_ = matrix_.n_ - 1;
+            }
+            return *this;
         }
-    }
-    else {
-        std::cerr << "Dictionary not specified" << std::endl;
-        return 0;
-    }
 
-    Mat camMatrix, distCoeffs;
-    if(estimatePose) {
-        bool readOk = readCameraParameters(parser.get<string>("c"), camMatrix, distCoeffs);
-        if(!readOk) {
-            cerr << "Invalid camera file" << endl;
-            return 0;
+        // Оператор разыменования для получения текущего элемента матрицы
+        double operator*() const {
+            return matrix_(row_, col_);
         }
+
+    private:
+        const VandermondeMatrix& matrix_;
+        size_t row_;
+        size_t col_;
+    };
+
+    // Методы для получения начального итератора итератора и окончания матрицы
+    ConstIterator begin() const {
+        return ConstIterator(*this, 0, 0);
     }
-    aruco::ArucoDetector detector(dictionary, detectorParams);
-    VideoCapture inputVideo;
-    int waitTime;
-    if(!video.empty()) {
-        inputVideo.open(video);
-        waitTime = 0;
-    } else {
-        inputVideo.open(camId);
-        waitTime = 10;
+
+    ConstIterator end() const {
+        return ConstIterator(*this, m_, 0); // Не включая последний элемент
     }
 
-    double totalTime = 0;
-    int totalIterations = 0;
+private:
+    vector<double> coefficients_;
+    size_t m_;
+    size_t n_;
+    vector<vector<double>> elements_;
 
-    // Set coordinate system
-    cv::Mat objPoints(4, 1, CV_32FC3);
-    objPoints.ptr<Vec3f>(0)[0] = Vec3f(-markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[1] = Vec3f(markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[2] = Vec3f(markerLength/2.f, -markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[3] = Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
-
-    while(inputVideo.grab()) {
-        Mat image, imageCopy;
-        inputVideo.retrieve(image);
-
-        double tick = (double)getTickCount();
-
-        vector< int > ids;
-        vector< vector< Point2f > > corners, rejected;
-
-        // detect markers and estimate pose
-        detector.detectMarkers(image, corners, ids, rejected);
-
-        size_t  nMarkers = corners.size();
-        vector<Vec3d> rvecs(nMarkers), tvecs(nMarkers);
-
-        if(estimatePose && !ids.empty()) {
-            // Calculate pose for each marker
-            for (size_t  i = 0; i < nMarkers; i++) {
-                solvePnP(objPoints, corners.at(i), camMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
+    // Метод для вычисления всех элементов матрицы Вандермонда
+    void computeElements() {
+        elements_.resize(m_, vector<double>(n_));
+        for (size_t i = 0; i < m_; ++i) {
+            for (size_t j = 0; j < n_; ++j) {
+                elements_[i][j] = pow(coefficients_[i], j);
             }
         }
+    }
+};
 
-        double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
-        totalTime += currentTime;
-        totalIterations++;
-        if(totalIterations % 30 == 0) {
-            cout << "Detection Time = " << currentTime * 1000 << " ms "
-                 << "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << endl;
-        }
+int main() {
+    vector<double> coefficients = {2, 3, 4}; // Пример коэффициентов αi
+    size_t m = 3; // Количество строк
+    size_t n = 4; // Количество столбцов
 
-        // draw results
-        image.copyTo(imageCopy);
-        if(!ids.empty()) {
-            aruco::drawDetectedMarkers(imageCopy, corners, ids);
+    VandermondeMatrix matrix(coefficients, m, n);
 
-            if(estimatePose) {
-                for(unsigned int i = 0; i < ids.size(); i++)
-                    cv::drawFrameAxes(imageCopy, camMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 1.5f, 2);
-            }
-        }
-
-        if(showRejected && !rejected.empty())
-            aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
-
-        imwrite("new_result.jpg", imageCopy);
-        imshow("out", imageCopy);
-
-        for (int i = 0; i < corners.size(); i++) {
-            cout << "Marker: " << i+1 << " {\n";
-            for (auto & j : corners[i]) {
-                cout << "\tPoint (" << j.x << ", " << j.y << ")" << endl;
-            }
-            cout << "}\n";
-        }
-
-        cout << "\nJSON format:\n\n";
-
-        string output = "[\n";
-        for (int i = 0; i < corners.size(); i++) {
-            for (int j = 0; j < 1; j++) {
-                output += "    ";
-            }
-            output += vectorToString(corners[i], 1);
-            if (i < corners.size() - 1) {
-                output += ",";
-            }
-            output += "\n";
-        }
-        output += "]";
-
-        cout << output << endl;
-
-
-
-        char key = (char)waitKey(waitTime);
-        if(key == 27) break;
+    // Итерирование по всем элементам матрицы с использованием итератора с начала
+    for (auto it = matrix.begin(); it != matrix.end(); ++it) {
+        cout << *it << " ";
     }
 
+    cout << endl;
+
+    // Итерирование по всем элементам матрицы с использованием итератора с конца
+    for (auto it = --matrix.end(); it != --matrix.begin(); --it) {
+        cout << *it << " ";
+    }
     return 0;
 }
